190defe5c2fcfcb9f14aab3a8474a34cc4760178
JardelBrandon <jardelbrandon@hotmail.com>
2017-12-04 11:42:18 -0200

    Questão 4
    Adicionando o histograma letra b) da questão 4, faltando ainda a letra a) que é a construção da distribuição de frequência de dados

--- Atividades/Tabelas e gráficos de distribuições/Questao4.py
+++ Atividades/Tabelas e gráficos de distribuições/Questao4.py
@@ -13,3 +13,18 @@ Os depósitos bancários de uma empresa, em milhares de reais, são dispostos na
 a) Construa a distribuição de frequências dos dados
 b) Construa o histograma
 '''
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+dados = [3.7,     1.6,     2.5,     3.0,     3.9,     1.9,     3.8,     1.5,     1.1,
+         1.8,     1.4,     2.7,     2.1,     3.3,     3.2,     2.3,     2.3,     2.4,
+         0.8,     3.1,     1.8,     1.0,     2.0,     2.0,     2.8,     3.2,     1.9,
+         1.6,     2.9,     2.0,     1.0,     2.7,     3.0,     1.3,     1.5,     4.2,
+         2.4,     2.1,     1.3,     2.7,     2.1,     2.8,     1.9,]
+
+histogram_example = plt.hist(dados, bins=15)
+
+plt.show()
