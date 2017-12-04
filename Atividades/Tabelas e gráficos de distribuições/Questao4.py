fb265aff4cfd93ba17acce02830be0b9225c47bb
JardelBrandon <jardelbrandon@hotmail.com>
2017-12-04 16:57:33 -0200

    Questão 4
    Adicionando resolução completa com os códigos da questão 4 do slide

--- Atividades/Tabelas e gráficos de distribuições/Questao4.py
+++ Atividades/Tabelas e gráficos de distribuições/Questao4.py
@@ -23,8 +23,28 @@ dados = [3.7,     1.6,     2.5,     3.0,     3.9,     1.9,     3.8,     1.5,    
          1.8,     1.4,     2.7,     2.1,     3.3,     3.2,     2.3,     2.3,     2.4,
          0.8,     3.1,     1.8,     1.0,     2.0,     2.0,     2.8,     3.2,     1.9,
          1.6,     2.9,     2.0,     1.0,     2.7,     3.0,     1.3,     1.5,     4.2,
-         2.4,     2.1,     1.3,     2.7,     2.1,     2.8,     1.9,]
+         2.4,     2.1,     1.3,     2.7,     2.1,     2.8,     1.9]
 
 histogram_example = plt.hist(dados, bins=15)
 
+def imprimirTabelaDeFrequencias(baseDeDados):
+    count, division = np.histogram(baseDeDados)
+    frequenciaAcumulada = 0
+    print("\t\t\t\t\t\t\t\t\t\tFrequências")
+    print("Classes:\t\t\t\tAbsoluta:\t\tRelativa:\t\tAcumulada:")
+    for indice in range(0, len(division)):
+        if indice + 1 == len(division):
+            break;
+        if indice + 2== len(division):
+            print('[', "{0:.2f}".format(division[indice]), '|--|',
+                  "{0:.2f}".format(division[indice + 1]), ']', end='\t\t\t')
+        else:
+            print('[', "{0:.2f}".format(division[indice]), '|-- ',
+                  "{0:.2f}".format(division[indice + 1]), ')', end='\t\t\t')
+        print(count[indice], end='\t\t\t  ')
+        print("{0:.2f}".format((count[indice]/sum(count))*100), end='\t\t\t\t')
+        frequenciaAcumulada += count[indice]
+        print(frequenciaAcumulada)
+
+imprimirTabelaDeFrequencias(dados)
 plt.show()
