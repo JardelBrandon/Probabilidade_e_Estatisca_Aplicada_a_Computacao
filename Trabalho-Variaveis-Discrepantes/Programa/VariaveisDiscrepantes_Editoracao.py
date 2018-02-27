eafef30092e355d963c2771375cfef80081de9eb
Jardel Brandon de Araujo Regis <jardelbrandon@hotmail.com>
2018-02-26 23:48:01 -0300

    Variaveis discrepantes
    Realizando desenvolvimento do projeto, foram convertidos os formatos dos
    arquivos para utf-8 e gerado um gráfico simples do tipo histograma

---
+++ Trabalho-Variaveis-Discrepantes/Programa/VariaveisDiscrepantes_Editoracao.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+apoio_editoracao = pd.read_csv('Editoracao.csv', sep=';')
+
+plt.hist(apoio_editoracao['Total Geral (R$)'], bins=15, orientation='horizontal')
+plt.title("Valores totais por projetos de editoração")
+plt.xlabel("Value")
+plt.ylabel("Frequency")
+plt.show()
