fbd80256abcb5d16ee35a574bf5bcdae12c0ca7e
ericguimaraesb <34292244+ericguimaraesb@users.noreply.github.com>
2018-02-28 07:57:16 -0300

    Add files via upload

---
+++ Trabalho-Variaveis-Discrepantes/Programa/BoxPlot_Editoracao.py
@@ -0,0 +1,14 @@
+# Boxplot da quantidade de propostas por ano, a nível nacional
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+editoracao = pd.read_csv('Editoracao.csv', encoding='utf-8', sep=';')
+
+editoracao.boxplot(column='Qtd Propostas', by='Ano Fase')
+p = plt.gca()
+p.set_ylim([0,40])
+plt.set_title('axes title')
+
+# Boxplot da quantidade de propostas por ano, a nível nacional
+
